ea294fdccfa3b3fa252d6e669e6547bb57c3e1a0
someshshivram <148188302+someshshivram@users.noreply.github.com>
2023-11-13 16:17:42 +0530

    Update datalake.tf
    xxx

--- datalake.tf
+++ datalake.tf
@@ -1,14 +1,22 @@
 provider "azurerm" {
-   features {}
+  features {}
   subscription_id   = "d115ca25-a8e8-4afd-960b-135f53fcd450"
   tenant_id         = "d1bd4750-0901-4d48-a8a0-bf61cbbd3a99"
   client_id         = "c7b7fe63-197b-488f-b59c-bc8c9e819811"
   client_secret     = "etL8Q~NWIjIR7u2d7mBkdtUMfkeqeHx1gKCu4dki"
 }
 
+# Define variables
+variable "resource_group_name" {
+  description = "AZAZmyResourceGroupUserAzureResourceGroup"
+  default     = "AZAZdatalakepocResourceGroupUser"
+}
+
+
+# Create an Azure Resource Group
 resource "azurerm_resource_group" "example" {
-  name     = "pocdatalakestorageAZscripts"
-  location = "East US"  # Change to your desired region
+  name     = var.resource_group_name
+  location = "East US" # Change this to your desired region
 }
 
 resource "azurerm_storage_account" "example" {
@@ -27,3 +35,5 @@ resource "azurerm_storage_container" "example" {
   storage_account_name  = azurerm_storage_account.example.name
   container_access_type = "private"
 }
+
+
